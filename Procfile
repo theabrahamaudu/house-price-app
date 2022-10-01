a30b3e966ec275e4349f70d71ab80b5b0efbfa16
Abraham Audu <ab6ril@gmail.com>
2022-10-01 17:12:07 +0100

    (On branch streamlit_version) updated setup.sh and Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app && sh setup.sh && streamlit run frontend/streamlit_FE.py
+web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker ./app/main:app && sh setup.sh && streamlit run frontend/streamlit_FE.py
--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
-run cd app
-run python -m main
-run cd ..
-mkdir -p ~frontend/.streamlit/
+mkdir -p ~/frontend/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.toml
+" > ~frontend/.streamlit/config.toml
